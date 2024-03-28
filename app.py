7abb5fe896dbac82679f0df3cdad4fb3ed8487be
GiovanniEstrada <cgiovanniestrada@gmail.com>
2024-03-28 15:20:24 -0600

    Funcionalidad de paginas web

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template, request
+app = Flask(__name__)
+listaUsuarios = []
+
+@app.route("/")
+def hello_world():
+    return render_template("login.html")
+
+@app.route("/POST", methods=['POST'])
+def insertarUsuario():
+    nombre = str(request.form.get('Nombre'))
+    correo = str(request.form.get('Correo'))
+    nit = str(request.form.get('Nit'))
+    error = ""
+
+    # SE VALIDA SI HAY CAMPOS VACIOS
+    if nit == "" or nit == None:
+        error = "Campo NIT del cliente vacío"
+        
+    if correo == "" or correo == None:
+        error = "Campo correo electrónico vacío"
+
+    if nombre == "" or nombre == None:
+        error = "Campo nombre cliente vacío"
+
+    # SE VALIDA QUE CLIENTE NO EXISTA
+    for usuario in listaUsuarios:
+        if str(usuario[2]) == str(nit):
+            error = "Usuario ya existe..."
+    
+    if error != "":
+        return render_template("login.html", error = error)
+
+    usuario = [request.form.get('Nombre'), request.form.get('Correo'), request.form.get('Nit')]
+    listaUsuarios.append(usuario)
+    return render_template("login.html", success = "¡Usuario ingresado con éxito!")
+
+@app.route("/LISTADO")
+def verUsuarios():
+    return render_template("listado.html", ListaUsuarios = listaUsuarios)
+
+
+if __name__ == '__main__':
+    app.run(port=1500, debug=True)
